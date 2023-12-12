6c88bec98f1c303682b37e854a990a1e8aa1d5cf
Sergi Muyo Abad <sergimuyo@gmail.com>
2023-12-12 22:39:38 +0000

    Day 9 finished That was easy

---
+++ 2023/code/day9_2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def step_compute(in_list):
+    new_list = []
+    if all(v == 0 for v in in_list):
+        cur_res = 0
+    else:
+        for pair in range(1, len(in_list)):
+            new_list.append(in_list[pair] - in_list[pair - 1])
+        
+        res_upper = step_compute(new_list)
+
+        cur_res = new_list[0] - res_upper
+    
+    return cur_res
+
+values = 0
+with open(sys.argv[1], "r") as in_file:
+    for line in in_file:
+        start_vals = list(map(int, line.strip().split()))
+        values += start_vals[0] - step_compute(start_vals)
+
+print(values)
